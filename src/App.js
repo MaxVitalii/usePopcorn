3b87cfe29e86fc3789aca7665450621b848ad41a
Vitalii Maksymenko <maxymenkovitaly@gmail.com>
2024-11-19 18:12:12 +0100

    Cleaning Up Data Fetching

--- src/App.js
+++ src/App.js
@@ -79,26 +79,31 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
             throw new Error("Something went wrong with fetching movies");
 
           const data = await res.json();
-
           if (data.Response === "False") throw new Error("Movie not found");
 
           setMovies(data.Search);
-          setIsLoading(false);
+          setError("");
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -111,6 +116,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
